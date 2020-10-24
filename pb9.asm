ab79abf6f1966cd5766fb0846754e1f4d551b07f
DianaIfrosa <40547787+DianaIfrosa@users.noreply.github.com>
2020-10-24 20:46:27 +0300

    Add files via upload

---
+++ pb9.asm
@@ -0,0 +1,43 @@
+.data 
+   n: .long 165
+   const: .long 0
+   
+.text
+.globl _start
+
+_start:
+   mov n, %eax
+   mov $0, %ecx
+   //cmp $0, %eax
+   jmp etdivizori 
+
+//etsemn:
+    // mov $-1, %ebx
+    // imul %ebx
+     //jmp etdivizori
+     
+     
+etgasit:
+   mov %ecx, %ebx   
+     
+etdivizori:
+
+  mov n, %eax
+ 
+  add $1, %ecx
+  cmp n, %ecx
+  jg etexit 
+  
+  mov $0, %edx
+  div %ecx
+  cmp %edx, const
+  je etgasit
+  
+  jmp etdivizori
+  
+etexit:
+ mov $1, %eax
+ mov $0, %ebx
+ int $0x80  
+       
+      
