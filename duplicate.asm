3363cfb4c88e974815bbc2096027ab43f6e6e6bf
Ifrosă Diana-Maria <40547787+DianaIfrosa@users.noreply.github.com>
2021-07-15 20:24:28 +0300

    Update duplicate.asm

--- duplicate.asm
+++ duplicate.asm
@@ -1,8 +1,9 @@
-//Enunt: Fie dat un array in memorie de lungime impara. Stim ca toate elementele sunt duplicate, cu
-//exceptia unuia, de exemplu v = { 1245, 342, 543523, 342, 4234, 1245, 543523 }, unde
-//elementul 4234 este elementul singular. Sa se scrie un algoritm eficient care sa determine acest
-//element (complexitate spatiu O(1) si complexitate timp O(n), unde n este lungimea array-
-//ului).
+//Problem: We are given an array with odd length. We know that all elements are duplicated, except for one, for example 
+// v = { 1245, 342, 543523, 342, 4234, 1245, 543523 }, where
+//element 4234 is not duplicated. Write an efficient algorithm to find that element in
+//space complexity O(1) and time complexity O(n), where n is the lenght of the array.
+
+//Idea: XOR
 
 .data
    v: .long 12, 13, 12, 5, 8, 13, 8  
